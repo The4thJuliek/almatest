0d39e135f5c2f30bce1c53dcc67b6ce0c4ef0fae
The4thJuliek <58864444+The4thJuliek@users.noreply.github.com>
2023-05-19 16:23:03 +0200

    commented code.

--- app.py
+++ app.py
@@ -11,18 +11,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xml'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
 
-@app.route("/hello")
-@app.route("/hello/<name>")
-def hello_there(name=None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
 
 @app.route("/api/data")
 def get_data():
