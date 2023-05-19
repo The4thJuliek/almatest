a97ab7ea11c34e930950e28100cfc5fb1ac6a2b9
RM <ragini.menon@hadw-bw.de>
2023-05-19 17:06:27 +0200

    many changes blah blah

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import re
 from datetime import datetime
 import os
-from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template
+
+# , flash, request, redirect, url_for, send_from_directory
+# from werkzeug.utils import secure_filename
 
 
 UPLOAD_FOLDER = '/Users/raginimenon/Desktop/alm'
@@ -11,13 +13,13 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xml'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
-
 # @app.route("/")
+# def home():
+#     return "Hello, Flask!"
+
+# @app.route("/hello/")
 # @app.route("/hello/<name>")
-# def hello_there(name=None):
+# def hello_there(name = None):
 #     return render_template(
 #         "hello_there.html",
 #         name=name,
@@ -33,32 +35,32 @@ def home():
 #     return '.' in filename and \
 #            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# #@app.route('/', methods=['GET', 'POST'])
-# # def upload_file():
-# #     if request.method == 'POST':
-# #         # check if the post request has the file part
-# #         if 'file' not in request.files:
-# #             flash('No file part')
-# #             return redirect(request.url)
-# #         file = request.files['file']
-# #         # If the user does not select a file, the browser submits an
-# #         # empty file without a filename.
-# #         if file.filename == '':
-# #             flash('No selected file')
-# #             return redirect(request.url)
-# #         if file and allowed_file(file.filename):
-# #             filename = secure_filename(file.filename)
-# #             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-# #             return redirect(url_for('download_file', name=filename))
-# #     return '''
-# #     <!doctype html>
-# #     <title>Upload new File</title>
-# #     <h1>Upload new File</h1>
-# #     <form method=post enctype=multipart/form-data>
-# #       <input type=file name=file>
-# #       <input type=submit value=Upload>
-# #     </form>
-# #     '''
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 # # @app.route('/uploads/<name>')
 # # def download_file(name):
