d6b57bc6444a5f220a31505376404ccd30a1823d
RM <ragini.menon@hadw-bw.de>
2023-05-23 13:13:16 +0200

    changed, added and modified app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-import re
-from datetime import datetime
-import os
-from flask import Flask, render_template
+# import re
+# from datetime import datetime
+# import os
+# from flask import Flask, render_template
 
-# , flash, request, redirect, url_for, send_from_directory
-# from werkzeug.utils import secure_filename
+# # , flash, request, redirect, url_for, send_from_directory
+# # from werkzeug.utils import secure_filename
 
 
-UPLOAD_FOLDER = '/Users/raginimenon/Desktop/alm'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xml'}
+# UPLOAD_FOLDER = '/Users/raginimenon/Desktop/alm'
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xml'}
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # @app.route("/")
 # def home():
-#     return "Hello, Flask!"
+    # return "Hello, Flask!"
 
 # @app.route("/hello/")
 # @app.route("/hello/<name>")
@@ -35,32 +35,32 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #     return '.' in filename and \
 #            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+# @app.route('/', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # If the user does not select a file, the browser submits an
+#         # empty file without a filename.
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             return redirect(url_for('download_file', name=filename))
+#     return '''
+#     <!doctype html>
+#     <title>Upload new File</title>
+#     <h1>Upload new File</h1>
+#     <form method=post enctype=multipart/form-data>
+#       <input type=file name=file>
+#       <input type=submit value=Upload>
+#     </form>
+#     '''
 
 # # @app.route('/uploads/<name>')
 # # def download_file(name):
@@ -91,3 +91,18 @@ def upload_file():
 
 # if __name__=="__main__":
 #     app.run(host='localhost', port=8888, debug=True)
+
+
+import os.path
+from flask import Flask
+from flask_autoindex import AutoIndex
+
+app = Flask(__name__)
+AutoIndex(app, browse_root=os.path.curdir)
+
+if __name__ == '__main__':
+    app.run()
+
+@app.route('/helloworld')
+def helloworld():
+    return 'Hello, world!', 200
